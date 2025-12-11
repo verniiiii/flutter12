fac3a0d2c433bb0d006b21a10e3762b5d4b286e3
Shestopalova Veronika <vera.shestopalova@gmail.com>
2025-12-11 19:42:01 +0300

    move legacy data sources to local folder

---
+++ lib/data/datasources/local/auth_local_datasource.dart
@@ -0,0 +1,18 @@
+import 'package:prac12/core/models/user_model.dart';
+
+class AuthLocalDataSource {
+  User? _currentUser;
+
+  Future<User?> getCurrentUser() async {
+    return _currentUser;
+  }
+
+  Future<void> setCurrentUser(User? user) async {
+    _currentUser = user;
+  }
+
+  Future<void> clearUser() async {
+    _currentUser = null;
+  }
+}
+
